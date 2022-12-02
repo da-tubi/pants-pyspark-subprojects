aa5b21c3aeed3bdfad010cbbda64edf58bac5658
Da Shen <da@tubi.tv>
2022-12-02 13:21:48 +0800

    Make tubi_project customizable

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -1,20 +1,20 @@
-def tubi_project(**kwargs):
-    name=kwargs['name']
-
+def tubi_project(name, customize_sources = False, customize_tests = False):
     python_requirements(
         name=f"{name}_requirements",
         source=f"tubi/{name}/requirements.txt",
         resolve=name,
     )
     
-    python_sources(
-        name=f"{name}_sources",
-        sources=[f"tubi/{name}/*.py"],
-        resolve=name,
-    )
+    if not customize_sources:
+        python_sources(
+            name=f"{name}_sources",
+            sources=[f"tubi/{name}/*.py"],
+            resolve=name,
+        )
     
-    python_tests(
-        name=f"{name}_tests",
-        sources=[f"tests/tubi/{name}/*.py"],
-        resolve=name,
-    )
+    if not customize_tests:
+        python_tests(
+            name=f"{name}_tests",
+            sources=[f"tests/tubi/{name}/*.py"],
+            resolve=name,
+        )
